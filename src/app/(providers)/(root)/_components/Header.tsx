a0e5b5a7e17f9ed284ddadb1d0b344edd8f86c22
im2hw <julisanta1015@gmail.com>
2024-01-11 15:38:35 +0900

    header 완성

--- src/app/(providers)/(root)/_components/Header.tsx
+++ src/app/(providers)/(root)/_components/Header.tsx
@@ -1,3 +1,25 @@
+import Link from 'next/link';
+import Navigation from './Navigation';
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="flex justify-between items-center border-b-2 p-4">
+      <section className="flex items-center justify-evenly w-1/2">
+        <div>
+          <Link href="/">
+            <h1 className="font-bold text-xl">What Surv?</h1>
+          </Link>
+        </div>
+        <Navigation />
+      </section>
+      <section className="w-1/2 flex justify-end gap-4">
+        <Link href="/join">
+          <button className="w-[80px] h-[40px] border-2 border-[#ddd] rounded-xl">Join</button>
+        </Link>
+        <Link href="/auth">
+          <button className="w-[80px] h-[40px] bg-[#00709F] rounded-xl text-white">Login</button>
+        </Link>
+      </section>
+    </header>
+  );
 }
