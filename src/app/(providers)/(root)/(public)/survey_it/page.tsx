a027f68d804949596d3eb8905ec09a908e4e4c11
kwanghee <rkdgh4427@gmail.com>
2024-01-11 15:10:41 +0900

    [ADD] it_page에 firebase 가져오기

--- src/app/(providers)/(root)/(public)/survey_it/page.tsx
+++ src/app/(providers)/(root)/(public)/survey_it/page.tsx
@@ -1,8 +1,39 @@
+'use client';
+
+import {fetchPosts} from '@/app/api/firebaseApi';
+import {Post} from '@/app/api/typePost';
+import {useQuery} from '@tanstack/react-query';
+
 export default function SurveyItPage() {
+  const {
+    data: surveyData,
+    isLoading,
+    isError,
+    refetch,
+  } = useQuery<Post[]>({
+    queryKey: ['surveyData'],
+    queryFn: fetchPosts,
+  });
+
   return (
-    <>
-      <p>설문조사</p>
-      <p>링크</p>
-    </>
+    <div>
+      <h1>Survey Data</h1>
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>Error fetching survey data</div>}
+      {surveyData && (
+        <ul>
+          {surveyData.map(item => (
+            <li key={item.id}>
+              <p>{item.category}</p>
+              <p>{item.createdAt.toDate().toLocaleString()}</p>
+              <p>{item.imageUrl}</p>
+              <p>{item.title}</p>
+              <p>{item.content}</p>
+              <p>{item.likes}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
