2760c3dcc4e2d8aec3a606b6eb668d6586af5cac
im2hw <julisanta1015@gmail.com>
2024-01-15 11:09:45 +0900

    tab bar 경로 수정

--- src/app/(providers)/(root)/_components/Tab.tsx
+++ src/app/(providers)/(root)/_components/Tab.tsx
@@ -4,16 +4,16 @@ export default function Tab() {
   return (
     <nav className="h-12 border-b-1">
       <ul className="flex items-center divide-x-2 text-center h-12 ml-[70px]">
-        <Link href="/survey_it">
+        <Link href="/survey-it">
           <li className="w-24 hover:text-[#0051FF]">IT</li>
         </Link>
-        <Link href="/survey_beauty">
+        <Link href="/survey-beauty">
           <li className="w-24 hover:text-[#0051FF]">BEAUTY</li>
         </Link>
-        <Link href="/survey_medical">
+        <Link href="/survey-medical">
           <li className="w-24 hover:text-[#0051FF]">MEDICAL</li>
         </Link>
-        <Link href="/survey_lite">
+        <Link href="/survey-lite">
           <li className="w-24 hover:text-[#0051FF]">LITE</li>
         </Link>
       </ul>
