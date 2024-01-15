65c878116371527e275451989586fb15fca3c414
chillakilla <doroome246@gmail.com>
2024-01-15 10:16:42 +0900

    create create-post/_components/PostForm.tsx

--- src/app/api/typePost.ts
+++ src/app/api/typePost.ts
@@ -1,20 +1,30 @@
 import {Timestamp} from 'firebase/firestore';
 
 export type Post = {
-  id: number;
   likes: number;
   views: number;
   rewards: number;
 
+  id: string;
   title: string;
   content: string;
   imageUrl: string;
   category: string;
-  userId: string;
-  userNickname: string;
   requirements: string;
 
   createdAt: Timestamp;
+  updatedAt: Timestamp;
   deadlineDate: Date;
   participationDate: Date;
 };
+
+interface PostFormProps {
+  formData: Omit<Post, 'views' | 'id' | 'createdAt' | 'updatedAt'> & {
+    deadlineDate: string;
+    participationDate: string;
+  };
+  onInputChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  onImgFileChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: (e: React.FormEvent) => void;
+  previewImage: string | null;
+}
