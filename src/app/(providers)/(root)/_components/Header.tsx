c29cfdd2d0c617964b1c1c1e45b5dd68fc2e6e96
im2hw <julisanta1015@gmail.com>
2024-01-16 10:42:43 +0900

    tab bar 스크롤시  숨기기

--- src/app/(providers)/(root)/_components/Header.tsx
+++ src/app/(providers)/(root)/_components/Header.tsx
@@ -3,23 +3,25 @@ import Tab from './Tab';
 
 export default function Header() {
   return (
-    <header>
-      <section className="flex justify-between items-center border-b-1 bg-white">
-        <div className="flex items-center justify-center w-80">
-          <Link href="/">
-            <h1 className="font-bold text-xl">What Surv?</h1>
-          </Link>
-        </div>
-        <div className="w-80 flex justify-center gap-4 p-2">
-          <Link href="/join">
-            <button className="w-[64px] h-[30px] border-2 border-[#ddd] rounded-full text-sm">Join</button>
-          </Link>
-          <Link href="/auth">
-            <button className="w-[64px] h-[30px] bg-[#0051FF] rounded-full text-white text-sm">Login</button>
-          </Link>
-        </div>
-      </section>
+    <div>
+      <header className="fixed top-0 w-full z-index-2">
+        <section className="flex justify-between items-center border-b-1 bg-white ">
+          <div className="flex items-center justify-center w-80">
+            <Link href="/">
+              <h1 className="font-bold text-xl">What Surv?</h1>
+            </Link>
+          </div>
+          <div className="w-80 flex justify-center gap-4 p-2">
+            <Link href="/join">
+              <button className="w-[64px] h-[30px] border-2 border-[#ddd] rounded-full text-sm">Join</button>
+            </Link>
+            <Link href="/auth">
+              <button className="w-[64px] h-[30px] bg-[#0051FF] rounded-full text-white text-sm">Login</button>
+            </Link>
+          </div>
+        </section>
+      </header>
       <Tab />
-    </header>
+    </div>
   );
 }
--- src/app/(providers)/(root)/_components/Tab.tsx
+++ src/app/(providers)/(root)/_components/Tab.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 export default function Tab() {
   return (
-    <nav className="h-12 border-b-1 bg-white">
+    <nav className=" hide-nav h-12 w-full border-b-1 bg-white transition-transform duration-1000 ease-in-out">
       <ul className="flex items-center divide-x-2 text-center h-12 ml-[70px]">
         <Link href="/survey-it">
           <li className="w-24 hover:text-[#0051FF]">IT</li>
