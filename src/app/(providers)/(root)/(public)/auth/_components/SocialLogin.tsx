acb0490ce46088519f3bdea919cd05c2a511667b
allone9425 <allone9425@gmail.com>
2024-01-22 09:54:39 +0900

    fix : 소셜로그인 진행 후 메인페이지 이동

--- src/app/(providers)/(root)/(public)/auth/_components/SocialLogin.tsx
+++ src/app/(providers)/(root)/(public)/auth/_components/SocialLogin.tsx
@@ -8,65 +8,67 @@ import {
   signInWithPopup,
 } from 'firebase/auth';
 import {doc, setDoc} from 'firebase/firestore';
+import {useRouter} from 'next/navigation';
 
-export const googleLogin = async () => {
-  try {
-    await setPersistence(auth, browserSessionPersistence); // 세션 지속성 설정
-    const provider = new GoogleAuthProvider();
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
-
-    // Firestore에 저장할 사용자 정보
-    const userData = {
-      nickname: user.displayName || '기본닉네임',
-      email: user.email,
-      // 구글API에서 생년월일은 미지원으로 생년월일은 초기에 빈 값으로 설정
-      birthdate: '',
-
-      sexType: '--미설정--',
-    };
-
-    // 사용자 정보 저장
-    await setDoc(doc(db, 'users', user.uid), userData);
-
-    alert('Google 로그인 성공!');
-  } catch (error) {
-    console.error('Google 로그인 실패:', error);
-  }
-};
-
-// GitHub 로그인 함수
-export const githubLogin = async () => {
-  try {
-    await setPersistence(auth, browserSessionPersistence); // 세션 지속성 설정
-    const provider = new GithubAuthProvider();
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
-
-    // 이메일이 없는 경우, 대체할 값을 사용
-    const userEmail = user.email || `${user.uid}@no-email.com`;
-
-    // Firestore에 저장할 사용자 정보
-    const userData = {
-      nickname: user.displayName || userEmail.split('@')[0], // GitHub username 또는 이메일을 사용
-      email: userEmail,
-      //생년월일은 초기에 빈 값으로 설정
-      birthdate: '',
-
-      sexType: '--미설정--',
-    };
-
-    // 사용자 정보 저장
-    await setDoc(doc(db, 'users', user.uid), userData);
-
-    alert('GitHub 로그인 성공!');
-  } catch (error) {
-    console.error('GitHub 로그인 실패:', error);
-  }
-};
 export default function SocialLogin() {
-  // Google 로그인 함수
+  const router = useRouter();
+  const googleLogin = async () => {
+    try {
+      await setPersistence(auth, browserSessionPersistence); // 세션 지속성 설정
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
 
+      // Firestore에 저장할 사용자 정보
+      const userData = {
+        nickname: user.displayName || '기본닉네임',
+        email: user.email,
+        // 구글API에서 생년월일은 미지원으로 생년월일은 초기에 빈 값으로 설정
+        birthdate: '',
+
+        sexType: '--미설정--',
+      };
+
+      // 사용자 정보 저장
+      await setDoc(doc(db, 'users', user.uid), userData);
+
+      alert('Google 로그인 성공!');
+      router.push('/');
+    } catch (error) {
+      console.error('Google 로그인 실패:', error);
+    }
+  };
+
+  // GitHub 로그인 함수
+  const githubLogin = async () => {
+    try {
+      await setPersistence(auth, browserSessionPersistence); // 세션 지속성 설정
+      const provider = new GithubAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // 이메일이 없는 경우, 대체할 값을 사용
+      const userEmail = user.email || `${user.uid}@no-email.com`;
+
+      // Firestore에 저장할 사용자 정보
+      const userData = {
+        nickname: user.displayName || userEmail.split('@')[0], // GitHub username 또는 이메일을 사용
+        email: userEmail,
+        //생년월일은 초기에 빈 값으로 설정
+        birthdate: '',
+
+        sexType: '--미설정--',
+      };
+
+      // 사용자 정보 저장
+      await setDoc(doc(db, 'users', user.uid), userData);
+
+      alert('GitHub 로그인 성공!');
+      router.push('/');
+    } catch (error) {
+      console.error('GitHub 로그인 실패:', error);
+    }
+  };
   return (
     <div className="text-center font-bold mt-[30px]">
       <p className="text-xl">간편 로그인</p>
