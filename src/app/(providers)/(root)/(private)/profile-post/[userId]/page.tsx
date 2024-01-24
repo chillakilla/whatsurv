5f7684fa386a9d190a625fd30a044e8d9d60fc7f
allone9425 <allone9425@gmail.com>
2024-01-24 15:45:27 +0900

    feat : 내가 작성한 서베이 탭 기능 구현

--- src/app/(providers)/(root)/(private)/profile-post/[userId]/page.tsx
+++ src/app/(providers)/(root)/(private)/profile-post/[userId]/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import {Button} from '@nextui-org/react';
 import Link from 'next/link';
 import {useParams} from 'next/navigation';
 import {useEffect, useState} from 'react';
@@ -15,16 +16,20 @@ interface PostLite {
   content: string;
 }
 
+type Tab = 'IT' | 'Lite';
+
 export default function ProfilePost() {
   const [posts, setPosts] = useState<PostIT[]>([]);
+  const [userPostLite, setUserPostLite] = useState<PostLite[]>([]);
   const params = useParams<{userId: string}>();
   const userId = params.userId;
-  const [userPostLite, setUserPostLite] = useState<PostLite[]>([]);
+
+  //탭 기능 관련 상태
+  const [currentTab, setCurrentTab] = useState('IT');
 
   useEffect(() => {
     if (userId) {
       getUserPostsIT(userId).then(data => {
-        // 기존 게시글 데이터 설정
         setPosts(
           data.map(doc => ({
             id: doc.id,
@@ -36,7 +41,6 @@ export default function ProfilePost() {
       });
 
       getUserPostLite(userId).then(data => {
-        // 새로운 게시글 데이터 설정
         setUserPostLite(
           data.map(doc => ({
             id: doc.id,
@@ -48,40 +52,47 @@ export default function ProfilePost() {
     }
   }, [userId]);
 
+  // 탭 변경 핸들러
+  const clickUserPostTabHandler = (tabName: Tab) => {
+    setCurrentTab(tabName);
+  };
+
   return (
     <div>
-      <h1>내가 작성한 IT 서베이</h1>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>
-            <Link href={`/survey-it/${post.id}`}>
-              {post.title}
-              <p>
-                마감일:
-                {post.deadlineDate
-                  ? post.deadlineDate.toLocaleDateString('ko-KR', {
-                      year: 'numeric',
-                      month: '2-digit',
-                      day: '2-digit',
-                    })
-                  : 'No deadline'}
-              </p>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <Button onClick={() => clickUserPostTabHandler('IT')}>내가 작성한 IT 서베이</Button>
+      <Button onClick={() => clickUserPostTabHandler('Lite')}>내가 작성한 참여했Surv</Button>
 
-      <h1>내가 작성한 참여했Surv</h1>
-      <ul>
-        {userPostLite.map(post => (
-          <li key={post.id}>
-            <Link href={`/litesurveyposts/${post.id}`}>
-              {post.title}
-              {/* 필요한 경우 마감일 등 추가 정보 렌더링 */}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {currentTab === 'IT' && (
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}>
+              <Link href={`/survey-it/${post.id}`}>
+                {post.title}
+                <p>
+                  마감일:{' '}
+                  {post.deadlineDate
+                    ? post.deadlineDate.toLocaleDateString('ko-KR', {
+                        year: 'numeric',
+                        month: '2-digit',
+                        day: '2-digit',
+                      })
+                    : 'No deadline'}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {currentTab === 'Lite' && (
+        <ul>
+          {userPostLite.map(post => (
+            <li key={post.id}>
+              <Link href="/survey-lite">{post.title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
