bfffd85b323ce7aaa5a95d5e210899be768f58cc
kwanghee <rkdgh4427@gmail.com>
2024-01-25 18:30:51 +0900

    [ADD] 게시물 출력 시 이미지 로딩 지연 로딩 중... 출력

--- src/app/(providers)/(root)/(main)/_components/modal/SurveyModal.tsx
+++ src/app/(providers)/(root)/(main)/_components/modal/SurveyModal.tsx
@@ -3,7 +3,7 @@
 import {litePost} from '@/app/api/typePost';
 import {db} from '@/firebase';
 import {doc, getDoc, updateDoc} from 'firebase/firestore';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Swal from 'sweetalert2';
 import ResultModal from './ResultModal';
 
@@ -18,6 +18,28 @@ const LiteSurveyModal: React.FC<LiteSurveyModalProps> = ({litepost, contents, on
   const [selectedContentIndex, setSelectedContentIndex] = useState<number | null>(null);
   const [contentsCounts, setContentsCounts] = useState<number[]>(new Array(contents.length).fill(0));
   const [showResultModal, setShowResultModal] = useState(false);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  useEffect(() => {
+    // 이미지 로딩이 완료되면 상태 변경
+    const images = litepost.images || [];
+    const imagePromises = images.map(src => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = resolve;
+        img.onerror = reject;
+      });
+    });
+
+    Promise.all(imagePromises)
+      .then(() => {
+        setImagesLoaded(true);
+      })
+      .catch(() => {
+        setImagesLoaded(true); // 또는 에러 처리
+      });
+  }, [litepost.images]);
 
   // 어떤 내용을 카운트 하는지 정하기
   const onClickContentsHandler = (index: number) => {
@@ -83,72 +105,76 @@ const LiteSurveyModal: React.FC<LiteSurveyModalProps> = ({litepost, contents, on
 
   return (
     <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50 flex items-center justify-center">
-      <div className="bg-white w-[39rem] p-8 rounded-lg">
-        <div className="mb-4 flex justify-center gap-4">
-          {litepost.images.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt={`Image ${index}`}
-              className="max-w-64 h-auto max-h-64 object-cover mb-2"
-            />
-          ))}
-        </div>
-        <div className="mb-2 flex justify-between items-center">
-          <span className="text-sm">조사기간 : 추가해야함</span>
-          <span className="text-sm">참여인원 : {totalVotes}명</span>
-        </div>
-        <h2 className="text-2xl font-bold mb-4 border-b border-black pb-4 mb-4">{litepost.title}</h2>
-        <div className="mb-4">
-          {contents.map((item, index) => (
-            <div
-              key={index}
-              className="flex items-center mb-2 cursor-pointer border-b border-blue-300 pb-4 mb-4"
-              onClick={() => onClickContentsHandler(index)}
+      {imagesLoaded ? (
+        <div className="bg-white w-[39rem] p-8 rounded-lg">
+          <div className="mb-4 flex justify-center gap-4">
+            {litepost.images.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`Image ${index}`}
+                className="max-w-64 h-auto max-h-64 object-cover mb-2"
+              />
+            ))}
+          </div>
+          <div className="mb-2 flex justify-between items-center">
+            <span className="text-sm">조사기간 : 추가해야함</span>
+            <span className="text-sm">참여인원 : {totalVotes}명</span>
+          </div>
+          <h2 className="text-2xl font-bold mb-4 border-b border-black pb-4 mb-4">{litepost.title}</h2>
+          <div className="mb-4">
+            {contents.map((item, index) => (
+              <div
+                key={index}
+                className="flex items-center mb-2 cursor-pointer border-b border-blue-300 pb-4 mb-4"
+                onClick={() => onClickContentsHandler(index)}
+              >
+                <input
+                  type="radio"
+                  name="contentRadioGroup"
+                  className={`w-4 h-4 rounded-full border border-blue-500 mr-2 ${
+                    selectedContentIndex === index ? 'bg-blue-500' : 'bg-white'
+                  }`}
+                ></input>
+                {item}
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-end mt-4">
+            <button
+              className="bg-[#4D85FF] text-white px-4 py-2 rounded-md mr-2 hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300"
+              onClick={onClickSurveySubmitHandler}
             >
-              <input
-                type="radio"
-                name="contentRadioGroup"
-                className={`w-4 h-4 rounded-full border border-blue-500 mr-2 ${
-                  selectedContentIndex === index ? 'bg-blue-500' : 'bg-white'
-                }`}
-              ></input>
-              {item}
-            </div>
-          ))}
+              참여하기
+            </button>
+            <button
+              className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md mr-2 hover:bg-gray-400 focus:outline-none focus:ring focus:border-gray-500"
+              onClick={() => {
+                setShowResultModal(true);
+              }}
+            >
+              결과보기
+            </button>
+            <button
+              className="bg-[#EB271C] text-white px-4 py-2 rounded-md hover:bg-red-700 focus:outline-none focus:ring focus:border-red-300"
+              onClick={onCloseLiteSurveyModal}
+            >
+              닫기
+            </button>
+            {/* 결과보기 모달 표시 */}
+            {showResultModal && (
+              <ResultModal
+                litepost={litepost}
+                contents={contents}
+                counts={contentsCounts}
+                onClickResultModalCloseHandler={resultModalClosehandler}
+              />
+            )}
+          </div>
         </div>
-        <div className="flex justify-end mt-4">
-          <button
-            className="bg-[#4D85FF] text-white px-4 py-2 rounded-md mr-2 hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300"
-            onClick={onClickSurveySubmitHandler}
-          >
-            참여하기
-          </button>
-          <button
-            className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md mr-2 hover:bg-gray-400 focus:outline-none focus:ring focus:border-gray-500"
-            onClick={() => {
-              setShowResultModal(true);
-            }}
-          >
-            결과보기
-          </button>
-          <button
-            className="bg-[#EB271C] text-white px-4 py-2 rounded-md hover:bg-red-700 focus:outline-none focus:ring focus:border-red-300"
-            onClick={onCloseLiteSurveyModal}
-          >
-            닫기
-          </button>
-          {/* 결과보기 모달 표시 */}
-          {showResultModal && (
-            <ResultModal
-              litepost={litepost}
-              contents={contents}
-              counts={contentsCounts}
-              onClickResultModalCloseHandler={resultModalClosehandler}
-            />
-          )}
-        </div>
-      </div>
+      ) : (
+        <div className="bg-white w-[39rem] h-[24rem] p-8 rounded-lg flex items-center justify-center">로딩 중...</div>
+      )}
     </div>
   );
 };
