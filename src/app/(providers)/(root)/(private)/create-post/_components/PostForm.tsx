78a0df4a5435701aa1fb5c8c7c61af84edc5326d
chillakilla <doroome246@gmail.com>
2024-01-25 19:18:01 +0900

    fix UI PostForm.tsx

--- src/app/(providers)/(root)/(private)/create-post/_components/PostForm.tsx
+++ src/app/(providers)/(root)/(private)/create-post/_components/PostForm.tsx
@@ -120,13 +120,13 @@ export default function PostForm({
         {/* 흰생 배경 컨테이너 */}
         <div className="w-[80rem] h-[109.375rem] mt-[5.5rem] bg-white flex flex-col justify-center items-center">
           {/* 문서 작성 컨테이너 */}
-          <div className="w-[74rem] h-[101.56rem] bg-gray-400">
+          <div className="w-[74rem] h-[101.56rem]">
             <form>
               {/* 타이틀 및 참여대상 연령 등 컨테이너 */}
-              <div className="w-[74rem] h-[6rem] flex bg-gray-300">
-                <div className="w-[54rem] h-[6rem] flex items-center">
+              <div className="w-[74rem] h-[6rem] flex ">
+                <div className="w-[74rem] h-[6rem] flex justify-center items-center  border-black border-b-2">
                   <input
-                    className="w-[54rem] h-[6rem]"
+                    className="w-[54rem] h-[5rem] p-2"
                     type="text"
                     name="title"
                     value={formData.title}
@@ -135,73 +135,109 @@ export default function PostForm({
                     required
                     placeholder="제목은 최대 70자까지 입력할 수 있습니다."
                   />
-                  <p className="text-[#818490]">{formData.title.length}/70</p>
+                  <p className="text-[#818490] p-2">{formData.title.length}/70</p>
                 </div>
-                <div className="flex w-[20rem] h-[6rem] justify-center items-center gap-3 bg-gray-100 ">
-                  <div className="flex flex-col gap-3">
-                    <select
-                      className="p-[2px] border border-sky-500 rounded-lg"
-                      name="sexType"
-                      value={formData.sexType}
-                      onChange={onCategoryChange}
-                      required
-                    >
-                      {sexType.map(option => (
-                        <option key={option.value} value={option.value}>
-                          {option.label}
-                        </option>
-                      ))}
-                    </select>
-                    <select
-                      className="p-[2px] border border-sky-500 rounded-lg"
-                      name="ageGroup"
-                      value={formData.ageGroup}
-                      onChange={onCategoryChange}
-                      required
-                    >
-                      {ageGroup.map(option => (
-                        <option key={option.value} value={option.value}>
-                          {option.label}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                  <div className="flex flex-col gap-3">
-                    <select
-                      className="p-[2px] border border-sky-500 rounded-lg"
-                      name="researchLocation"
-                      value={formData.researchLocation}
-                      onChange={onCategoryChange}
-                      required
-                    >
-                      {researchLocation.map(option => (
-                        <option key={option.value} value={option.value}>
-                          {option.label}
-                        </option>
-                      ))}
-                    </select>
-                    <select
-                      className="p-[2px] border border-sky-500 rounded-lg"
-                      name="researchTime"
-                      value={formData.researchTime}
-                      onChange={onCategoryChange}
-                      required
-                    >
-                      {researchTime.map(option => (
-                        <option key={option.value} value={option.value}>
-                          {option.label}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
+              </div>
+              <div>
+                <div className="flex w-[74rem] h-[6rem] justify-center items-center gap-3">
+                  <select
+                    className="p-[2px] border border-sky-500 rounded-lg"
+                    name="sexType"
+                    value={formData.sexType}
+                    onChange={onCategoryChange}
+                    required
+                  >
+                    {sexType.map(option => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </select>
+                  <select
+                    className="p-[2px] border border-sky-500 rounded-lg"
+                    name="ageGroup"
+                    value={formData.ageGroup}
+                    onChange={onCategoryChange}
+                    required
+                  >
+                    {ageGroup.map(option => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </select>
+                  <select
+                    className="p-[2px] border border-sky-500 rounded-lg"
+                    name="category"
+                    value={formData.category}
+                    onChange={onCategoryChange}
+                    required
+                  >
+                    {majorCategories.map(option => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </select>
+                  <select
+                    className="p-[2px] border border-sky-500 rounded-lg"
+                    name="researchType"
+                    value={formData.researchType}
+                    onChange={onCategoryChange}
+                    required
+                  >
+                    {researchType.map(option => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </select>
+                  <select
+                    className="p-[2px] border border-sky-500 rounded-lg"
+                    name="researchLocation"
+                    value={formData.researchLocation}
+                    onChange={onCategoryChange}
+                    required
+                  >
+                    {researchLocation.map(option => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </select>
+                  <select
+                    className="p-[2px] border border-sky-500 rounded-lg"
+                    name="researchTime"
+                    value={formData.researchTime}
+                    onChange={onCategoryChange}
+                    required
+                  >
+                    {researchTime.map(option => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </select>
+                  {/* <input
+                    className="p-[2px] border border-sky-500 rounded-lg"
+                    type="date"
+                    name="deadlineDate"
+                    value={
+                      formData.deadlineDate instanceof Date ? formData.deadlineDate.toISOString().split('T')[0] : ''
+                    }
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => onDateChange(e)}
+                  /> */}
                 </div>
               </div>
               {/* 설문조사 설명 컨테이너 */}
-              <div className="w-[74rem] h-[10.6875rem] mt-[1.75rem] flex flex-col items-center justify-center bg-gray-200">
-                <input
-                  type="text"
-                  className="w-[56.5625rem] h-[4.25rem] p-[1rem] bg-blue-300"
-                  placeholder="설문조사에 대한 설명을 작성해주세요."
+              <div className="w-[74rem] h-[10.6875rem] mt-[1.75rem] flex flex-col items-center justify-center border-1 border-gray-300">
+                <textarea
+                  className="w-[56.5625rem] h-[7.25rem] p-[1rem] rounded-lg resize-none"
+                  name="content"
+                  value={formData.content}
+                  onChange={onInputChange}
+                  required
+                  placeholder="설문조사에 대한 설명을 간단하게 작성해주세요."
                 />
               </div>
               {/* 설문조사 폼 양식 컨테이너 */}
@@ -218,73 +254,6 @@ export default function PostForm({
     //   <div className="w-[80rem] h-[109.375rem] bg-white">
     //     <div className="mx-[5rem] mt-[12.8rem] mb-[2.88rem]">
     //       <form onSubmit={onSubmit} className="mt-[0.5rem]">
-    //         <div className="w-[74rem] h-[6rem]">
-    //           <input
-    //             className="w-[54rem] h-[6rem] text-base font-semibold"
-    //             type="text"
-    //             name="title"
-    //             value={formData.title}
-    //             onChange={onInputChange}
-    //             maxLength={70}
-    //             required
-    //             placeholder="제목은 최대 70자까지 입력할 수 있습니다."
-    //           />
-    //           <p className="text-[#818490]">{formData.title.length}/70</p>
-    //           <div className="w-[20rem] h-[6rem] flex flex-col gap-[0.5rem]">
-    //             <select
-    //               className="p-[2px] border border-sky-500 rounded-lg"
-    //               name="sexType"
-    //               value={formData.sexType}
-    //               onChange={onCategoryChange}
-    //               required
-    //             >
-    //               {sexType.map(option => (
-    //                 <option key={option.value} value={option.value}>
-    //                   {option.label}
-    //                 </option>
-    //               ))}
-    //             </select>
-    //             <select
-    //               className="p-[2px] border border-sky-500 rounded-lg"
-    //               name="ageGroup"
-    //               value={formData.ageGroup}
-    //               onChange={onCategoryChange}
-    //               required
-    //             >
-    //               {ageGroup.map(option => (
-    //                 <option key={option.value} value={option.value}>
-    //                   {option.label}
-    //                 </option>
-    //               ))}
-    //             </select>
-    //           </div>
-    //           <div className="flex flex-col">
-    //             <select
-    //               className="p-[2px] border border-sky-500 rounded-lg"
-    //               name="researchLocation"
-    //               value={formData.researchLocation}
-    //               onChange={onCategoryChange}
-    //               required
-    //             >
-    //               {researchLocation.map(option => (
-    //                 <option key={option.value} value={option.value}>
-    //                   {option.label}
-    //                 </option>
-    //               ))}
-    //             </select>
-    //             <Input
-    //               className="p-[3px] border border-sky-500 rounded-lg"
-    //               type="text"
-    //               label="소요시간"
-    //               name="researchTime"
-    //               value={formData.researchTime}
-    //               onChange={onInputChange}
-    //               required
-    //               placeholder="설문에 소요되는 시간을 작성해주세요."
-    //             />
-    //           </div>
-    //         </div>
-    //         <Spacer y={6} />
     //         {/* <div className="flex items-start self-stretch w-[64.625rem]">
     //             <div className="flex justify-center items-center">
     //               <BsPersonCircle />
@@ -295,41 +264,9 @@ export default function PostForm({
     //           </div> */}
     //         <Spacer y={8} />
     //         <div className="flex items-center justify-between gap-[0.625rem]">
-    //           <select
-    //             className="p-[2px] border border-sky-500 rounded-lg"
-    //             name="category"
-    //             value={formData.category}
-    //             onChange={onCategoryChange}
-    //             required
-    //           >
-    //             {majorCategories.map(option => (
-    //               <option key={option.value} value={option.value}>
-    //                 {option.label}
-    //               </option>
-    //             ))}
-    //           </select>
-    //           <select
-    //             className="p-[2px] border border-sky-500 rounded-lg"
-    //             name="researchType"
-    //             value={formData.researchType}
-    //             onChange={onCategoryChange}
-    //             required
-    //           >
-    //             {researchType.map(option => (
-    //               <option key={option.value} value={option.value}>
-    //                 {option.label}
-    //               </option>
-    //             ))}
-    //           </select>
+    //
 
     //           <label htmlFor="deadlineDate">마감일: </label>
-    //           <input
-    //             className="p-[2px] border border-sky-500 rounded-lg"
-    //             type="date"
-    //             name="deadlineDate"
-    //             value={formData.deadlineDate instanceof Date ? formData.deadlineDate.toISOString().split('T')[0] : ''}
-    //             onChange={(e: React.ChangeEvent<HTMLInputElement>) => onDateChange(e)}
-    //           />
     //         </div>
     //         <Spacer y={6} />
     //         <div className="flex flex-col">
@@ -371,13 +308,7 @@ export default function PostForm({
     //             }}
     //           /> */}
     //           <label>내용: </label>
-    //           <textarea
-    //             className="border border-sky-500 rounded-lg resize-none"
-    //             name="content"
-    //             value={formData.content}
-    //             onChange={onInputChange}
-    //             required
-    //           />
+    //
     //         </div>
     //         {/* TODO: 새로 추가된 문항과 그에 따른 옵션 */}
     //         <div className="flex flex-col w-[64.625rem]">
