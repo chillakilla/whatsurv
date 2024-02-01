055ce9ceab7aded5cbedd0816abf8b2d497791cc
chillakilla <doroome246@gmail.com>
2024-02-01 20:31:36 +0900

    edit page fix

--- src/app/(providers)/(root)/(private)/edit-post/[id]/page.tsx
+++ src/app/(providers)/(root)/(private)/edit-post/[id]/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function EditPostFormPage() {
-  return <div>아직 구현되지 않았습니다.</div>;
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <div className="w-6/12 h-6/12 flex justify-center items-center">
+        <h1 className="text-3xl">🚧&nbsp;아직 구현되지 않았습니다.&nbsp;🚧</h1>
+      </div>
+    </div>
+  );
 }
